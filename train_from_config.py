cac98bd67e2f692c398b32e3f25febf8d5ee6492
TTomilin <tristan.tomilin@hotmail.com>
2025-11-11 00:48:53 +0100

    More aggressive lambda

--- train_from_config.py
+++ train_from_config.py
@@ -829,8 +829,8 @@ def main():
 
     # --- PPO-Lagrange ---
     parser.add_argument("--safety_bound", type=float, default=25.0, help="Episodic safety constraint bound")
-    parser.add_argument("--lagrangian_coef_rate", type=float, default=0.001, help="Lagrange multiplier LR")
-    parser.add_argument("--initial_lambda_lagr", type=float, default=0.0, help="Initial lambda value")
+    parser.add_argument("--lagrangian_coef_rate", type=float, default=0.01, help="Lagrange multiplier LR")
+    parser.add_argument("--initial_lambda_lagr", type=float, default=0.1, help="Initial lambda value")
 
     # --- PPO-Cost verification ---
     parser.add_argument("--ppoc-verify-log-steps", type=int, default=0,
